7b7c39dae706aa7dfbf630336e530534780d73d2
Leo Picado <lpicado@backcountry.com>
2011-12-20 18:59:13 -0600

    Refactor ready.

--- jquery.detectBrowser.sansjQuery.js
+++ jquery.detectBrowser.sansjQuery.js
@@ -1,64 +1,68 @@
-function getOs() {
-    var platform = window.navigator.platform || '',
-        os;
+(function(browserDetect) {
 
-    if (/win/i.test(platform)) {
-        os = 'win';
-    }
-    if (/mac/i.test(platform)) {
-        os = 'mac';
-    }
+    function getOs(navigator) {
+        var platform = navigator.platform || '',
+            os;
 
-    return os;
-}
-;
+        if (/win/i.test(platform)) {
+            os = 'win';
+        }
+        if (/mac/i.test(platform)) {
+            os = 'mac';
+        }
 
-function getBrowserName() {
-    var wn = window.navigator,
-        userAgent = wn.userAgent,
-        vendor = wn.vendor || '',
-        browserName;
+        return os;
+    };
 
-    if (/firefox/i.test(userAgent)) {
-        browserName = 'ff';
-    }
-    if (/msie/i.test(userAgent)) {
-        browserName = 'ie';
-    }
-    if (/apple/i.test(vendor)) {
-        browserName = 'sfr';
-    }
-    if (/google/i.test(vendor)) {
-        browserName = 'crm';
-    }
+    function getBrowserName(navigator) {
+        var userAgent = navigator.userAgent,
+            vendor = navigator.vendor || '',
+            browserName;
+
+        if (/firefox/i.test(userAgent)) {
+            browserName = 'ff';
+        }
+        if (/msie/i.test(userAgent)) {
+            browserName = 'ie';
+        }
+        if (/apple/i.test(vendor)) {
+            browserName = 'sfr';
+        }
+        if (/google/i.test(vendor)) {
+            browserName = 'crm';
+        }
 
 
-    return browserName;
-}
-;
+        return browserName;
+    };
 
-function getVersion() {
-    var userAgent = window.navigator.userAgent.toLowerCase(),
-        storedName,
-        version = '',
-        browserName = getBrowserName(),
-        regexpMap = { ff:  / Firefox\/(\d+\.\d+)/i,
-                      ie:  / MSIE (\d+\.\d+);/i,
-                      sfr: / Version\/(\d+\.\d+) /i,
-                      crm: / Chrome\/(\d+\.\d+)\./i },
-        versionRegex = regexpMap[browserName];
+    function getVersion(navigator) {
+        var userAgent = navigator.userAgent,
+            storedName,
+            version = '',
+            browserName = getBrowserName(navigator),
+            regexpMap = { ff:  / Firefox\/(\d+\.\d+)/i,
+                          ie:  / MSIE (\d+\.\d+);/i,
+                          sfr: / Version\/(\d+\.\d+) /i,
+                          crm: / Chrome\/(\d+\.\d+)\./i },
+            versionRegex = regexpMap[browserName];
 
-    if (versionRegex){
-        storedName = userAgent.match(versionRegex);
-        version = storedName && storedName[1];
-    }
+        if (versionRegex) {
+            storedName = userAgent.match(versionRegex);
+            version = storedName && storedName[1];
+        }
 
-    return version.replace(/\./, '');
-}
-;
+        return version.replace(/\./, '');
+    };
 
-var browserDetect = function() {
+    window[browserDetect] = function(navigator) {
+        navigator || (navigator = window.navigator);
 
-    document.body.className += ' ' + getOs() + ' ' + getBrowserName() + ' ' + getBrowserName() + getVersion();
+        var os = getOs(navigator),
+            browserName = getBrowserName(navigator),
+            version = getVersion(navigator);
 
-};
+        document.body.className += ' ' + os + ' ' + browserName + ' ' + browserName + version;
+    };
+
+})('browserDetect');
